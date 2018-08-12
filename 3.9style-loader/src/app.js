3fb3abaf3dd56250f67098bc6c27f4f5894f4bde
MrZWH <1875290692@qq.com>
2018-08-13 01:27:42 +0800

    处理css style-loader

---
+++ 3.9style-loader/src/app.js
@@ -0,0 +1,14 @@
+import './css/base.css'
+import './css/common.css'
+// import base './css/base.css'
+
+// var flag = false
+
+// setInterval(function () {
+// 	if (flag) {
+// 		base.unuse()
+// 	} else {
+// 		base.use()
+// 	}
+// 	flag = !flag
+// }, 500)
